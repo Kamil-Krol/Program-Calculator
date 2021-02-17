dabb778e1bc1adfefdbdb0644daa4d00f08dc60b
Maciej Rudnicki <maciejru@amazon.com>
2021-02-17 18:31:01 +0100

    Add constructor

--- src/main/java/com/kodilla/MyService.java
+++ src/main/java/com/kodilla/MyService.java
@@ -5,4 +5,9 @@ public class MyService {
     private int someValue;
 
     private double someOtherValue;
+
+    public MyService(int someValue, double someOtherValue) {
+        this.someValue = someValue;
+        this.someOtherValue = someOtherValue;
+    }
 }
